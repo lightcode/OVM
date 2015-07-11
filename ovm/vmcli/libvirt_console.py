2957222620e1de5ceab28528b97fab19844d2488
lightcode <lightcode@gmx.com>
2015-07-12 00:30:24 +0200

    Refactor the console command

--- ovm/vmcli/libvirt_console.py
+++ ovm/vmcli/libvirt_console.py
@@ -21,10 +21,11 @@
 
 
 import atexit
-import os
-import tty
 import libvirt
+import os
+import sys
 import termios
+import tty
 
 from ovm.inventory import Inventory
 
@@ -37,19 +38,9 @@ def error_handler(_, error):
 
 
 class Console:
+
     def __init__(self, domain_name):
-        libvirt.virEventRegisterDefaultImpl()
-        libvirt.registerErrorHandler(error_handler, None)
-
-        atexit.register(self.reset_term)
-        self.attrs = termios.tcgetattr(0)
-        tty.setraw(0)
-
         self.domain_name = domain_name
-        self.connection = Inventory.new_connection()
-        self.domain = self.connection.lookupByName(domain_name)
-        self.state = self.domain.state(0)
-        self.connection.domainEventRegister(self.lifecycle_callback, self)
         self.stream = None
         self.run_console = True
 
@@ -57,50 +48,64 @@ class Console:
         termios.tcsetattr(0, termios.TCSADRAIN, self.attrs)
 
     def start(self):
-        self.stdin_watch = libvirt.virEventAddHandle(
-            0, libvirt.VIR_EVENT_HANDLE_READABLE, self.stdin_callback, self)
+        libvirt.virEventRegisterDefaultImpl()
+        libvirt.registerErrorHandler(error_handler, None)
 
-        print("Escape character is ^]")
+        atexit.register(self.reset_term)
+        self.attrs = termios.tcgetattr(0)
+        tty.setraw(0)
+
+        self.connection = Inventory.new_connection()
+        self.domain = self.connection.lookupByName(self.domain_name)
+        self.state = self.domain.state(0)
+        self.connection.domainEventRegister(self.lifecycle_callback, self)
+
+        sys.stdout.write('Press Control+] to quit.\n\r')
+        sys.stdout.flush()
+
+        libvirt.virEventAddHandle(
+            0, libvirt.VIR_EVENT_HANDLE_READABLE, self.stdin_callback, None)
 
         while self.check_console():
             libvirt.virEventRunDefaultImpl()
 
+        sys.stdout.write('\n\rExited.\n\r')
+        sys.stdout.flush()
+
     def check_console(self):
-        if (self.state[0] == libvirt.VIR_DOMAIN_RUNNING or
-           self.state[0] == libvirt.VIR_DOMAIN_PAUSED):
-            if self.stream is None:
-                self.stream = self.connection.newStream(
-                    libvirt.VIR_STREAM_NONBLOCK)
-                self.domain.openConsole(None, self.stream, 0)
-                self.stream.eventAddCallback(
-                    libvirt.VIR_STREAM_EVENT_READABLE, self.stream_callback,
-                    self)
-        elif self.stream:
-            self.stream.eventRemoveCallback()
-            self.stream = None
+        if self.state[0] != libvirt.VIR_DOMAIN_RUNNING:
+            self.stream.finish()
+            return False
+
+        if self.stream is None:
+            self.stream = self.connection.newStream(
+                libvirt.VIR_STREAM_NONBLOCK)
+            self.domain.openConsole(None, self.stream)
+            self.stream.eventAddCallback(
+                libvirt.VIR_STREAM_EVENT_READABLE, self.stream_callback, None)
 
         return self.run_console
 
     def lifecycle_callback(self, connection, domain, event, detail, _):
-        self.state = self.domain.state(0)
+        self.state = domain.state(0)
 
     def stream_callback(self, stream, events, _):
         try:
-            received_data = self.stream.recv(1024)
+            received_data = stream.recv(1024)
         except:
             return
         os.write(0, received_data)
 
     def stdin_callback(self, watch, fd, events, _):
         readbuf = os.read(fd, 1024)
-        # Close connection when we receive a ^[
+        # Close connection when we receive a ^]
         if readbuf == b'\x1d':
             self.run_console = False
             return
         if self.stream:
             self.stream.send(readbuf)
 
-    @classmethod
-    def open_console(cls, domain_name):
+    @staticmethod
+    def open_console(domain_name):
         console = Console(domain_name)
         console.start()
